e88378ebf9c7fe172332fbee0fd03d75d95664b1
ryuoryuo <invfalle@gmail.com>
2019-04-30 01:59:15 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack"); //to access built-in plugins
+
+module.exports = {
+  entry: ["./src/index"],
+  bail: true,
+  mode: process.env.NODE_ENV || "development",
+  devtool: process.env.NODE_ENV ? false : "eval-source-map",
+  output: {
+    pathinfo: process.env.NODE_ENV || true,
+    filename: process.env.NODE_ENV
+      ? "dist/[name].[chunkhash:8].js"
+      : "dist/main.js",
+    chunkFilename: process.env.NODE_ENV
+      ? "dist/[name].[chunkhash:8].chunk.js"
+      : "dist/[name].chunk.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["react-hot-loader/webpack", "babel-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: "./src",
+    hot: true
+  },
+
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    },
+    runtimeChunk: true
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
