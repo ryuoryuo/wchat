09271d933a926ced21ab3335f8fd2a6341873043
ryuoryuo <invfalle@gmail.com>
2019-05-03 20:24:33 +0300

    added font

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { hot } from "react-hot-loader/root";
 import React from "react";
+import { Normalize } from "styled-normalize";
+import { createGlobalStyle } from "styled-components";
 
 import { AppContent } from "./components/AppContent";
 
-import { Normalize } from "styled-normalize";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+   font-family: Roboto, sans-serif;
+  }
+`;
 
 const App = () => (
   <>
+    <GlobalStyle />
     <Normalize />
     <AppContent />
   </>
