d4b4a308a3cd402ab616206dac0f17cc623a4493
ryuoryuo <invfalle@gmail.com>
2019-05-08 02:25:55 +0300

    eslint update

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -3,5 +3,61 @@ module.exports = {
 
   globals: {
     socket: true
+  },
+  rules: {
+    "no-plusplus": [
+      "error",
+      {
+        allowForLoopAfterthoughts: true
+      }
+    ],
+    "consistent-return": [
+      "error",
+      {
+        treatUndefinedAsUnspecified: true
+      }
+    ],
+    "no-use-before-define": 0,
+    "react/no-unescaped-entities": "off",
+    "jsx-a11y/anchor-is-valid": "off",
+    "no-confusing-arrow": "off",
+    "implicit-arrow-linebreak": "off",
+    "arrow-parens": [2, "as-needed"],
+    "no-shadow": "off",
+    "no-underscore-dangle": "off",
+    "no-unused-vars": "error",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: true
+      }
+    ],
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: ["const", "let", "var"],
+        next: "*"
+      },
+      {
+        blankLine: "any",
+        prev: ["const", "let", "var"],
+        next: ["const", "let", "var"]
+      }
+    ],
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always"
+      }
+    ],
+    "import/newline-after-import": [
+      "warn",
+      {
+        count: 2
+      }
+    ],
+    quotes: [1, "double"],
+    "import/prefer-default-export": "off"
   }
 };
