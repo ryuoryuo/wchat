003fd3304de1f4866a5779319aa3ad140db393c9
ryuoryuo <invfalle@gmail.com>
2019-05-13 00:55:39 +0300

    deleted users fetcg

--- src/components/AppContent/UsersList/UsersList.js
+++ src/components/AppContent/UsersList/UsersList.js
@@ -21,18 +21,6 @@ export const UsersList = () => {
     // });
   }, []);
 
-  const fetchUsers = async () => {
-    try {
-      const response = await fetch("http://localhost:3000/users");
-
-      const data = await response.json();
-
-      setUsers(data);
-    } catch (error) {
-      console.log(error, "error");
-    }
-  };
-
   return (
     <UsersContainer>
       {users.map(item => (
